9e3a183bfc2d9dd31e0e247934714c49a9be4580
Meraj Ali <merajali45425@gmail.com>
2023-12-17 21:56:20 -0800

    first commit

---
+++ src/AppRouter/AppRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from '../Components/Dashboard/Dashboard'
+import SignIn from '../Components/SignIn/Login/SignIn'
+const AppRoute = () => {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<SignIn />} />
+                <Route path='/dashboard/*' element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoute
